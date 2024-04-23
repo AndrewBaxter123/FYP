e7f98b3e1cd50b2fdbe9dfa00ef29f6819384551
Andrew Baxter <74902103+AndrewBaxter123@users.noreply.github.com>
2024-04-23 14:34:27 +0100

    Adding license and final version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# FYP
-Steganography Detection Plugin for Autopsy
+# FYP - Steganography Detection Plugin for Autopsy
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.txt](LICENSE.txt) file for details.
+
+
+
--- src/org/andrewbaxter/SteganographyDetection/scripts/LSB.py
+++ src/org/andrewbaxter/SteganographyDetection/scripts/LSB.py
@@ -1,14 +1,26 @@
-# this code does a few things
-# - converts an image to png  format from jpeg or jpg
-# - analyses the images using chi-square statistics of the LSBs
-# - threshold is currently set to 70000 (giving 60-70% success rate in detecting stego in stego images & 40% success rate in detecting stego in clean images)
-# - outputs to the user in detail of which specific image is detected with stego along with accuracy percentage in detecting it
+"""
+MIT License
 
-# - works with images
-#	grayscale
-#	512x512
-#	no noise & lossy compression
-#	PNG
+Copyright (c) 2024 Andrew Baxter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 import os
 import numpy as np
